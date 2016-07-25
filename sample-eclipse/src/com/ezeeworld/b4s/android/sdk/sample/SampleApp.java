69a79ad5fb2e3bfa81130bf6a9c8f8496287173e
freboursier-ez <francois.reboursier@ezeeworld.com>
2016-07-25 09:55:38 +0200

    Publish SDK

--- sample-eclipse/src/com/ezeeworld/b4s/android/sdk/sample/SampleApp.java
+++ sample-eclipse/src/com/ezeeworld/b4s/android/sdk/sample/SampleApp.java
@@ -4,8 +4,10 @@ import android.app.Application;
 
 import com.ezeeworld.b4s.android.sdk.B4SAlertBehaviours;
 import com.ezeeworld.b4s.android.sdk.B4SSettings;
+import com.ezeeworld.b4s.android.sdk.B4SUserProperty;
+import com.ezeeworld.b4s.android.sdk.BuildConfig;
 import com.ezeeworld.b4s.android.sdk.monitor.MonitoringManager;
-import com.ezeeworld.b4s.android.sdk.server.InteractionsApi;
+import com.ezeeworld.b4s.android.sdk.notifications.NotificationService;
 
 /**
  * An example application for the B4S SDK that sets up some non-default settings and ensures the monitoring service is properly set up.
@@ -20,21 +22,25 @@ public class SampleApp extends Application {
 		B4SSettings settings = B4SSettings.init(this, "MY-APP-ID");
 
 		// Enable push messages
-		settings.setPushMessagingSenderId("MY-GOOGLE-SENDER-ID");
+//		settings.setPushMessagingSenderId("MY-GOOGLE-SENDER-ID");
 
 		// Adjust the settings to our needs
-		settings.setShouldLogScanning(true);
-		settings.setShouldLogMatching(true);
+		settings.setShouldLogScanning(BuildConfig.DEBUG);
+		settings.setShouldLogMatching(BuildConfig.DEBUG);
+
+		// Send deep links to our broadcast receiver (instead of the default launcher activity delivery)
+		NotificationService.registerDeepLinkStyle(NotificationService.DeepLinkStyle.BroadcastReceiver);
 
 		// Have the SDK manage warnings for Bluetooth, Play Services installation and Location Services
 		B4SAlertBehaviours.get().warnForBluetooth(true, 1, 2, 0); // Skip first time, then every other app launch
 		B4SAlertBehaviours.get().warnForGeolocation(true, 0, 1, 2); // Max 2 times
 		B4SAlertBehaviours.get().warnForPlayServices(true, 0, 0, 0); // One time only warning
 
-		if (settings.getCustomerFirstName() == null) {
-			// Still need to set the customer details
-			settings.storeCustomerFields("SampleApp", "", InteractionsApi.B4SGENDER_UNDEFINED, "SampleApp", "Demo", "", "", "", "", "", "", 0);
-		}
+
+		B4SUserProperty.get().store(B4SUserProperty.USER_FIRST_NAME, "Jean-Michel");
+		B4SUserProperty.get().store(B4SUserProperty.USER_LAST_NAME, "Bécatresse");
+		B4SUserProperty.get().store(B4SUserProperty.USER_GENDER, B4SUserProperty.Gender.Male);
+
 
 		// Start the monitoring service, if needed
 		MonitoringManager.ensureMonitoringService(this);
